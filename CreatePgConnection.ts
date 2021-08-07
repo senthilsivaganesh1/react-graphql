38a82159c5825c8b84bf450740e84c799c365981
Owner-PC <senthilsivaganesh@gmail.com>
2021-08-07 11:17:27 +0530

    updated URI to string

--- CreatePgConnection.ts
+++ CreatePgConnection.ts
@@ -3,7 +3,7 @@ import { Hotel } from "./Hotel";
 
 createConnection({
     type:"postgres",
-    url:process.env.DATABASE_URI,
+    url:"postgres://dbkyztfcakprkv:4bb9b385d7c5b1a3ad8fd78e7f26500f0207d42e367e5cd30f6298d7dd1bbf2d@ec2-54-147-93-73.compute-1.amazonaws.com:5432/de2pkfbunr66e3",
     ssl:true,
     entities:[Hotel],
     extra: {
