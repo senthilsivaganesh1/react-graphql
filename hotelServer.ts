33f0f6e7a7c215fc665056bea9ee3c4308a3d108
Owner-PC <senthilsivaganesh@gmail.com>
2021-08-07 11:22:35 +0530

    updated port to 3002

--- hotelServer.ts
+++ hotelServer.ts
@@ -24,7 +24,7 @@ router.all("/graphql", koaGraphQl({
 }))
 app.use(router.routes()).use(router.allowedMethods());
 apolloServer.applyMiddleware({app, cors:false});
-app.listen(3000);
+app.listen(3002);
 }
 
 main();
